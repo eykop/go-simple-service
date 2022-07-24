7ec8de6dbba5a062cd8b2f3964c64b49d72dca91
eykop <e.kopty@yahoo.com>
2022-07-24 10:57:33 +0200

    refactor(main): cleanup and tidy code
    move middleware(s) to dedicated package

---
+++ middlewares/product_middleware.go
@@ -0,0 +1,40 @@
+package middlewares
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"simplems/handlers"
+
+	"go.uber.org/zap"
+)
+
+func DecodeProductMiddleware(l *zap.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodPost || r.Method == http.MethodPut {
+				l.Debug("Request", zap.String("url", r.RequestURI), zap.String("method", r.Method), zap.Bool("decoding", true))
+				prod := handlers.JsonToProduct(r, l)
+				if prod == nil {
+					l.Error("Failed to read produc, could not decode product from json.")
+					http.Error(w, "Failed to deserialize product from json.", http.StatusBadRequest)
+					return
+				}
+
+				validationErr := prod.Validate()
+				if validationErr != nil {
+					l.Error("Failed to validate produc.", zap.Error(validationErr))
+					http.Error(w, fmt.Sprintf("Failed to validate product %s.", validationErr), http.StatusBadRequest)
+					return
+				}
+
+				l.Debug("decoded product info", zap.String("name", prod.Name))
+				// add the product to the context
+				ctx := context.WithValue(r.Context(), handlers.ProductKey{}, prod)
+				r = r.WithContext(ctx)
+			}
+			// Call the next handler, which can be another middleware in the chain, or the final handler.
+			next.ServeHTTP(w, r)
+		})
+	}
+}
